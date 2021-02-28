94c98d0f34a36aa51996d94cc009456f4ad5e03e
Michael Penn <mwpenn94@gmail.com>
2021-02-28 03:18:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,11 +13,11 @@ class Doctor
   end
   
   def new_appointment(date, patient)
-    Appointment.new(date, patient)
+    Appointment.new(date, patient, self)
   end
   
   def appointments
-    Appointment.all.select {|a| a.doctor == self.name}
+    Appointment.all.select {|a| a.doctor == self}
   end
   
   def patients
